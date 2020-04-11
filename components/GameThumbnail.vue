9776510dec866c753c0e8857b4f7f3e7c074216f
Markus Appel <markospoparkos@gmail.com>
2020-04-11 10:14:24 +0200

    Populate games section (imgs to be improved)

---
+++ components/GameThumbnail.vue
@@ -0,0 +1,70 @@
+<template>
+  <a :href="href" target="_blank">
+    <h2>{{ heading }}</h2>
+    <img :src="imgSrc" :alt="imgAlt" />
+  </a>
+</template>
+
+<script>
+export default {
+  props: {
+    heading: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
+    imgAlt: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+a {
+  @include link-reset;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  h2 {
+    font-family: $font-games;
+    font-weight: normal;
+  }
+
+  img {
+    width: 100%;
+    margin-top: 1rem;
+    box-shadow: 4px 4px 8px 4px darkgray;
+  }
+}
+
+@media (min-width: $min-desktop) {
+  a {
+    h2 {
+      transition: color 0.25s;
+    }
+    &:hover {
+      > h2 {
+        color: $color-highlight;
+      }
+      > img {
+        filter: none;
+      }
+    }
+
+    img {
+      filter: grayscale(100%);
+      transition: filter 0.5s;
+    }
+  }
+}
+</style>
--- pages/games.vue
+++ pages/games.vue
@@ -1,15 +1,106 @@
 <template>
   <div id="games">
     <h1>Games</h1>
+    <div class="thumbnails">
+      <GameThumbnail v-bind="breakout" />
+      <GameThumbnail v-bind="battleship" />
+      <GameThumbnail v-bind="evolife" />
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import GameThumbnail from '@/components/GameThumbnail'
+import imgBreakout from '@/assets/images/breakout.png'
+import imgBattleship from '@/assets/images/battleship.png'
+import imgEvolife from '@/assets/images/evolife.png'
+
+const imgData = {
+  breakout: {
+    heading: 'Breakout',
+    href: 'games/breakout',
+    imgSrc: imgBreakout,
+    imgAlt: 'Breakout',
+  },
+  battleship: {
+    heading: 'Battleship',
+    href: 'http://www.battleship-online.com',
+    imgSrc: imgBattleship,
+    imgAlt: 'Battleship',
+  },
+  evolife: {
+    heading: 'Evolife',
+    href: 'games/evolife',
+    imgSrc: imgEvolife,
+    imgAlt: 'Evolife',
+  },
+}
+
+export default {
+  components: { GameThumbnail },
+  data() {
+    return imgData
+  },
+}
 </script>
 
 <style lang="scss" scoped>
-h1 {
-  text-align: center;
+#games {
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    text-align: center;
+    font-family: $font-games;
+    font-weight: normal;
+    font-size: 3rem;
+    margin-top: 2rem;
+  }
+
+  .thumbnails {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    > * {
+      width: 70%;
+      margin-top: 3.5rem;
+      &:first-of-type {
+        margin-top: 2.25rem;
+      }
+    }
+  }
+}
+
+@media (min-width: $min-tablet) {
+  #games {
+    .thumbnails {
+      flex-direction: row;
+      justify-content: space-evenly;
+      flex-wrap: wrap;
+
+      > * {
+        width: 35%;
+        margin-top: 3rem;
+        &:first-of-type {
+          margin-top: 3rem;
+        }
+      }
+    }
+  }
+}
+
+@media (min-width: $min-desktop) {
+  #games {
+    .thumbnails {
+      > * {
+        width: 20%;
+        margin-top: 2rem;
+        &:first-of-type {
+          margin-top: 2rem;
+        }
+      }
+    }
+  }
 }
 </style>
