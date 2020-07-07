08237be12a7c6b9044b717baff44f2b480fcd681
Markus Appel <markospoparkos@gmail.com>
2020-07-07 21:05:14 +0200

    Add current song player interface

---
+++ components/Player.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="container">
+    <div class="song-info">
+      <div>Artist: {{ songData ? songData.artist : '-' }}</div>
+      <div>Album: {{ songData ? songData.album : '-' }}</div>
+      <div>Song: {{ songData ? songData.name : '-' }}</div>
+    </div>
+    <Icon
+      class="prev-btn amplitude-prev"
+      :class="{ disabled: !songData || songData.isFirst }"
+      :data-amplitude-playlist="playlistKey"
+      name="prev"
+    />
+    <Icon
+      class="play-pause-btn amplitude-play-pause"
+      :class="{ disabled: !songData }"
+      :data-amplitude-playlist="playlistKey"
+      :name="songData && playing ? 'pause' : 'play'"
+    />
+    <Icon
+      class="next-btn amplitude-next"
+      :class="{ disabled: !songData || songData.isLast }"
+      :data-amplitude-playlist="playlistKey"
+      name="next"
+    />
+  </div>
+</template>
+
+<script>
+import Icon from '@/components/Icon'
+
+export default {
+  components: {
+    Icon,
+  },
+  props: {
+    playlistKey: {
+      type: String,
+      default: null,
+    },
+    songData: {
+      type: Object,
+      default: null,
+    },
+    playing: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  flex-shrink: 0;
+  text-align: center;
+
+  .play-pause-btn,
+  .prev-btn,
+  .next-btn {
+    font-size: 3rem;
+    &.disabled {
+      pointer-events: none;
+      opacity: 0.5;
+    }
+  }
+}
+</style>
--- pages/drums.vue
+++ pages/drums.vue
@@ -1,34 +1,55 @@
 <template>
   <section class="page drums">
-    <div v-for="(playlist, key) of playlists" :key="playlist.title">
-      <h2>{{ playlist.title }}</h2>
-      <ol>
-        <li
-          v-for="(song, idx) of playlist.songs"
-          :key="song.name"
-          class="amplitude-play-pause"
-          :data-amplitude-song-index="idx"
+    <div class="songs">
+      <div v-for="(playlist, key) of playlists" :key="playlist.title">
+        <h2
+          class="amplitude-play"
           :data-amplitude-playlist="key"
+          :data-amplitude-song-index="0"
         >
-          <Icon :name="playingSongId === songId(key, idx) ? 'pause' : 'play'" />
-          {{ song.name }}
-        </li>
-      </ol>
+          {{ playlist.title }}
+        </h2>
+        <ol>
+          <li
+            v-for="(song, idx) of playlist.songs"
+            :key="song.name"
+            class="amplitude-play-pause"
+            :data-amplitude-song-index="idx"
+            :data-amplitude-playlist="key"
+          >
+            <Icon
+              :name="playingSongId === songId(key, idx) ? 'pause' : 'play'"
+            />
+            {{ song.name }}
+          </li>
+        </ol>
+      </div>
     </div>
+
+    <Player
+      :song-data="activeSongData"
+      :playlist-key="activePlaylistKey"
+      :playing="!!playingSongId"
+    />
   </section>
 </template>
 
 <script>
+import { pick } from 'lodash-es'
 import playlists from '@/assets/audio'
 import Icon from '@/components/Icon'
+import Player from '@/components/Player'
 
 export default {
   components: {
+    Player,
     Icon,
   },
   data() {
     return {
       playingSongId: null,
+      activeSongData: null,
+      activePlaylistKey: null,
     }
   },
   created() {
@@ -41,6 +62,7 @@ export default {
       callbacks: {
         play: this.onPlay,
         pause: this.onPause,
+        song_change: this.onSongChange,
       },
     })
   },
@@ -52,12 +74,43 @@ export default {
       this.playingSongId = null
     },
     onPlay() {
-      const playlistKey = this.$amplitude.getActivePlaylist()
-      const songIdx = this.$amplitude.getActivePlaylistMetadata().active_index
-      this.playingSongId = this.songId(playlistKey, songIdx)
+      const playlist = this.playlistData()
+      this.playingSongId = this.songId(playlist.key, playlist.active_index)
+    },
+    onSongChange() {
+      const playlist = this.playlistData()
+      const songData = this.$amplitude.getActiveSongMetadata()
+
+      this.activePlaylistKey = playlist.key
+      this.activeSongData = {
+        ...pick(songData, ['artist', 'album', 'name']),
+        isFirst: playlist.active_index === 0,
+        isLast: playlist.active_index === playlist.songs.length - 1,
+      }
+    },
+    playlistData() {
+      return {
+        ...this.$amplitude.getActivePlaylistMetadata(),
+        key: this.$amplitude.getActivePlaylist(),
+      }
     },
   },
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+section {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  height: 100%;
+
+  .songs {
+    flex-grow: 1;
+
+    > div {
+      margin-bottom: 1rem;
+    }
+  }
+}
+</style>
